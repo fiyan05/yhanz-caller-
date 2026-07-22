8005c36ea8116ba361ec4a2e9d9aaa33432eb683
fiyan05 <p14504686@gmail.com>
2026-07-22 22:35:31 +0700

    Add game selection and purchase functions

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+// ===============================
+// YHANZ TOPUP V1
+// ===============================
 
+let selectedGame = "";
+let selectedNominal = "";
+
+function pilihGame(game) {
+    selectedGame = game;
+    alert("Game dipilih: " + game);
+}
+
+function pilihNominal(nominal) {
+    selectedNominal = nominal;
+    alert("Nominal dipilih: " + nominal);
+}
+
+function beliSekarang() {
+
+    const userId = document.getElementById("userid").value;
+    const zoneId = document.getElementById("zoneid").value;
+
+    if(userId==""){
+        alert("Masukkan User ID");
+        return;
+    }
+
+    if(selectedGame==""){
+        alert("Pilih Game");
+        return;
+    }
+
+    if(selectedNominal==""){
+        alert("Pilih Nominal");
+        return;
+    }
+
+    alert(
+`========================
+YHANZ TOPUP
+
+Game : ${selectedGame}
+
+User ID : ${userId}
+
+Zone ID : ${zoneId}
+
+Nominal : ${selectedNominal}
+
+Terima kasih sudah order.
+========================`
+);
+
+}
